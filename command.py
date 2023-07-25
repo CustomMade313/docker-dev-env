b1d7916d35599081ffe5fb0622234e2b417dd2e2
CustomMade <TaxiarchisKO@gmail.com>
2023-07-25 18:26:06 +0300

    edited command.py to execute mysql queries inside docker container

--- command.py
+++ command.py
@@ -4,12 +4,17 @@ import sys
 COMMANDS = {
     'id' : 'docker ps -aqf "name={0}"',
     # docker-compose exec  {0} -> container-name {1} -> command and args
-    'exec': 'docker-compose exec {0} {1}'
+    'exec': 'docker-compose exec {0} {1}',
 }
 
 ARTISAN_PATH = '/var/www/artisan'
-# test.py php artisan migrate
 
+# TODO: find a better way to load credentials
+MYSQL_USERNAME = 'root'
+MYSQL_PASSWORD = 'secret'
+
+# "docker-compose exec mysql mysql -u {0} -p{1}  -e '{2};'"
+# 
 commnad_name = ''
 args = ''
 
@@ -40,9 +45,9 @@ else:
     for item in args:
         str_args += "{0} ".format(item)
 
-if command_name == "mysql":
-    str_args += ';'
 
-print(command_name)
-print(str_args)
+if command_name == "mysql": 
+    #  'exec-query':"docker-compose exec mysql mysql -u {0} -p{1}  -e '{2};'",
+    str_args = "-u {0} -p{1}  -e '{2};'".format(MYSQL_USERNAME, MYSQL_PASSWORD, str_args)
+   
 os.system(COMMANDS['exec'].format(command_name, "{0} {1}".format(command_name, str_args)))
